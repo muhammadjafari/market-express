6b52d111ae950bff8492fee0096fa329b7666d92
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-01-19 23:15:01 +0330

    route fixed

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/addProduct", (req, res) => {
+router.get("/add-product", (req, res) => {
   res.send("admin page");
 });
 
