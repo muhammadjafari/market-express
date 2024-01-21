0b7509771d5aa73cd057a3e34b9ca25d78c53e3e
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-01-21 22:41:51 +0330

    mvc pattern added

--- controllers/product.js
+++ controllers/product.js
@@ -1,15 +1,17 @@
-const products = [];
+const Product = require("../models/single-product");
 
 module.exports.addProductPage = (req, res) => {
   res.render("add-product");
 };
 
 module.exports.addProduct = (req, res) => {
-  products.push({ title: req.body.title });
+  const products = new Product(req.body.title);
+  products.saveProductData();
   res.redirect("/");
 };
 
 module.exports.getAllProducts = (req, res) => {
+  const products = Product.fetchAllProducts();
   res.render("shop", {
     pageTitle: "shop",
     products: products,
